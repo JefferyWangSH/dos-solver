fix: correct mistakes in formula and update results

src/main.py
@@ -56,14 +56,14 @@ if "__main__":
 
     # setting up params for grids
     lattice_size = 100
-    freq_range = [-12.0, 12.0]
+    freq_range = [-6.0, 6.0]
     freq_num = int(1e3)
 
     # some comments for the strategy of choicing the imaginary value:
     # a large imaginary value tends to smooth out the effect of finite lattice size, thus leading to a smooth dos curve.
     # hence, as the imaginary valude decreases, the lattice size should be correspondingly increased 
     # to avoid the sharp-peak behaviour of results.
-    infinitesimal_imag = 0.05
+    infinitesimal_imag = 0.08
 
     # setting up model params
     # choice free lattice model as our 0th order results of pertubation theory.
@@ -77,10 +77,10 @@ if "__main__":
     fermi_surface = 0.0
 
     # the gap should be sufficent low compared with hopping constant, such that the pertubation theory works.
-    static_gap = 0.2
+    static_gap = 0.1
     corr_length = float(0.1*lattice_size)
 
-    corr_length_range = list(lattice_size * np.array([ 0.0, 1.0, 2.0, 3.0, 4.0, 8.0, 16.0, 24.0 ]))
+    corr_length_range = list(lattice_size * np.array([ 0.0, 0.05, 0.1, 1.0, 2.0 ]))
     data_list = []
     for id, corr_length in enumerate(corr_length_range):
         data_list.append(dos_run(id))
@@ -97,10 +97,10 @@ if "__main__":
         plt.plot(omega_list, dos_list, label="${\\xi/L}$ = "+str_corr_per_length)
     plt.xlabel("${\omega/4t}$", fontsize=13)
     plt.ylabel("${N(\omega)}$", fontsize=13)
-    plt.title("${L = 100 \\times 100}$,  ${\Delta_{0}/t = 0.2}$")
+    plt.title("${L = 100 \\times 100}$,  ${\Delta_{0}/t = 0.1}$")
     plt.tight_layout()
     plt.legend(fontsize=12)
-    plt.savefig("./version_discrete/results/out.pdf")
+    plt.savefig("./results/phase_disordered_sc.pdf")
     plt.show()
 
 

src/model.py
@@ -15,7 +15,8 @@ class FreePropagator:
         self._hopping = 1.0
         self._chemical_potential = 1.0
         self._infinitesimal_imag = 1.0
-        self._arrary2d = np.zeros((self.MomentumDim(), self.FrequencyDim()), dtype=complex)
+        self._arrary2d_particle = np.zeros((self.MomentumDim(), self.FrequencyDim()), dtype=complex)
+        self._arrary2d_hole = np.zeros((self.MomentumDim(), self.FrequencyDim()), dtype=complex)
     
     def SetModelParms(self, hopping, chemical_potential) -> None:
         self._hopping = hopping
@@ -29,9 +30,10 @@ class FreePropagator:
         # use broadcast property of numpy and accelate the creation of free propagator matrix
         tmp_ek = np.array(np.mat([ k.energy(self._hopping, self._chemical_potential) for k in self._momentum_grids.MomentumGrids() ]).transpose())
         tmp_omega = self._freq_grids.Grids() + self._infinitesimal_imag * 1.0j
+        self._arrary2d_particle = (tmp_omega - tmp_ek)**-1
         # the plus sign here indicates that,
-        # there exist a partical-hole symmetry in our model of phase-disordered supercondutivity.
-        self._arrary2d = (tmp_omega + tmp_ek)**-1
+        # there exist a particle-hole symmetry in our model of phase-disordered supercondutivity.
+        self._arrary2d_hole = (tmp_omega + tmp_ek)**-1
 
     def FrequencyDim(self) -> int:
         return self._freq_grids.GridsNum()
@@ -39,8 +41,11 @@ class FreePropagator:
     def MomentumDim(self) -> int:
         return self._momentum_grids.GridsNum()
 
-    def Mat(self):
-        return self._arrary2d
+    def ParticleMat(self):
+        return self._arrary2d_particle
+
+    def HoleMat(self):
+        return self._arrary2d_hole
 
     # TODO: reload [][] operator
 
@@ -72,9 +77,9 @@ class Kernel:
         tmp_py = np.array([ k.data()[1] for k in self._momentum_grids.MomentumGrids() ])
         tmp_kx = np.array(np.mat(tmp_px).transpose())
         tmp_ky = np.array(np.mat(tmp_py).transpose())
-        self._array2d = ( ((tmp_kx+tmp_px) - 2*np.pi*((tmp_kx+tmp_px+np.pi)//(2*np.pi)))**2 
-                        + ((tmp_ky+tmp_py) - 2*np.pi*((tmp_ky+tmp_py+np.pi)//(2*np.pi)))**2 )**0.5
-        self._array2d = (1+(self._array2d*self._corr_length)**2)**-1.5 * np.pi/2 * (self._static_gap*self._corr_length)**2 / self.Dim()
+        self._array2d = ((tmp_kx+tmp_px) - 2*np.pi*((tmp_kx+tmp_px+np.pi)//(2*np.pi)))**2 \
+                      + ((tmp_ky+tmp_py) - 2*np.pi*((tmp_ky+tmp_py+np.pi)//(2*np.pi)))**2
+        self._array2d = (1+self._array2d*(self._corr_length)**2)**-1.5 * 2*np.pi * (self._static_gap*self._corr_length)**2 / self.Dim()
     
     def Dim(self) -> int:
         return self._momentum_grids.GridsNum()
@@ -104,10 +109,10 @@ class GreenFunc:
 
 
     def ComputeSelfEnergy(self) -> None:
-        self._array2d_self_energy = self._kernel.Mat().dot(self._free_propagator.Mat())
+        self._array2d_self_energy = self._kernel.Mat().dot(self._free_propagator.HoleMat())
 
     def ComupteGreenFunc(self) -> None:
-        self._array2d_green_func = self._free_propagator.Mat() / (1-self._free_propagator.Mat()*self._array2d_self_energy)
+        self._array2d_green_func = self._free_propagator.ParticleMat() / (1-self._free_propagator.ParticleMat()*self._array2d_self_energy)
 
     def SelfEnergyMat(self):
         return self._array2d_self_energy